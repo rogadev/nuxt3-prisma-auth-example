25a0edbc16413bd5a9f7a372363bfe16bd4cf8f8
Roga <ryanroga@gmail.com>
2023-03-20 20:05:03 -0700

    Basic Setup
    Installed TailwindCSS, Tailwind Typography, daisyUI, and Nuxt-Icon.
    
    Added a theme toggler and setup the themes to toggle.

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import './assets/css/tailwind.css';
+const availableThemes = {
+  light: 'cupcake',
+  dark: 'dark',
+};
+const colorMode = useColorMode();
+const theme = ref(colorMode.value === 'light' ? availableThemes.light : availableThemes.dark);
+watch(colorMode, () => {
+  theme.value = colorMode.value === 'dark' ? availableThemes.light : availableThemes.dark;
+});
+useHead({
+  htmlAttrs: computed(() => ({
+    'data-theme': theme.value
+  }))
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="prose m-4">
+    <h1>Welcome</h1>
+    <p>This is a very simple preview.</p>
+    <ThemeToggle />
+  </div>
+</template>
